f494a1a2f05630c0f964ce2492bc29b7c45036a9
rdsq <62446963+rdsq@users.noreply.github.com>
2025-04-04 15:49:09 +0300

    warning if not compiled

--- prompt.zsh
+++ prompt.zsh
@@ -1,5 +1,11 @@
 _BLAZESH_DIR="${0:A:h}"
+_BLAZESH_BIN_PATH="$_BLAZESH_DIR/target/release/blazesh"
 
-precmd() {
-    PS1="$($_BLAZESH_DIR/target/release/blazesh $? &)"
-}
+if [ -f "$_BLAZESH_BIN_PATH" ]; then
+    precmd() {
+        PS1="$($_BLAZESH_BIN_PATH $? &)"
+    }
+else
+    echo "blazesh: could not find the binary. You can compile it by using this command:" >&2
+    echo "cd $_BLAZESH_DIR && cargo build --release" >&2
+fi
