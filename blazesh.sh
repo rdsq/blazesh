1a792557af24ec323dea0462da6a1fe2c329d808
rdsq <62446963+rdsq@users.noreply.github.com>
2025-04-06 14:07:56 +0300

    and seems like done

--- blazesh.sh
+++ blazesh.sh
@@ -1,3 +1,9 @@
+_blazesh_construct_prompt () {
+    local exit_code="$?"
+    local jobs="$(jobs | wc -l)"
+    PS1=$($_BLAZESH_BIN_PATH "$exit_code" "$jobs")
+}
+
 if [ -n "$BASH_VERSION" ]; then
     export _BLAZESH_DIR="$(dirname "${BASH_SOURCE[0]}")"
     export _BLAZESH_SHELL=bash
@@ -8,14 +14,19 @@ else
     echo "blazesh: unknown shell" >&2
     return 1
 fi
-export _BLAZESH_BIN_PATH="$_BLAZESH_DIR/target/release/blazesh"
+if [ ! -n "$_BLAZESH_BIN_PATH" ]; then
+    # allow to set custom bin path
+    export _BLAZESH_BIN_PATH="$_BLAZESH_DIR/target/release/blazesh"
+fi
 
 if [ -f "$_BLAZESH_BIN_PATH" ]; then
-    precmd() {
-        local exit_code="$?"
-        local jobs="$(jobs | wc -l)"
-        PS1=$($_BLAZESH_BIN_PATH "$exit_code" "$jobs")
-    }
+    if [ -n "$BASH_VERSION" ]; then
+        PROMPT_COMMAND="$PROMPT_COMMAND; _blazesh_construct_prompt"
+    elif [ -n "$ZSH_VERSION"]; then
+        precmd() {
+            _blazesh_construct_prompt
+        }
+    fi
 else
     echo "blazesh: could not find the binary. You can compile it by using this command:" >&2
     echo "cd $_BLAZESH_DIR && cargo build --release" >&2
