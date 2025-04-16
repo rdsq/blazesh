fcc03a690be86439c873028d9d140352a18bdf76
rdsq <62446963+rdsq@users.noreply.github.com>
2025-04-16 16:20:32 +0000

    fix jobs display

--- blazesh.sh
+++ blazesh.sh
@@ -1,6 +1,6 @@
 _blazesh_construct_prompt () {
     local exit_code="$?"
-    local jobs="$(jobs | wc -l)"
+    local jobs="$(jobs -p | grep '^\[' | wc -l)"
     PS1=$($_BLAZESH_BIN_PATH "$exit_code" "$jobs")
 }
 
