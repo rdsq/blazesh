9a39aa8a438e12f07f0e869ac789a55c18bf75bd
rdsq <62446963+rdsq@users.noreply.github.com>
2025-05-19 14:30:01 +0300

    fix negative exit codes

--- src/setup/snippets/construct-prompt.sh
+++ src/setup/snippets/construct-prompt.sh
@@ -1,5 +1,5 @@
 _blazesh_construct_prompt () {
     local exit_code="$?"
     local jobs="$(jobs -p | grep '^\[' | wc -l)"
-    PS1=$(blazesh prompt "$exit_code" "$jobs")
+    PS1=$(blazesh prompt -- "$exit_code" "$jobs")
 }
