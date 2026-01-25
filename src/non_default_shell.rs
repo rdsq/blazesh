3fcac5b206a917a96f63d13cd0e66449174d2321
rdsq <rdsq@proton.me>
2026-01-25 17:01:20 +0200

    improve non default shell accuracy

--- src/non_default_shell.rs
+++ src/non_default_shell.rs
@@ -1,9 +1,34 @@
 use std::env;
 use crate::colors::escseq::EscSeqFormat;
 use std::path::Path;
+use std::io::{prelude::*, BufReader};
+
+fn get_default_shell_from_env() -> Option<String> {
+    // doesn't work as default shell in some environments
+    // but rather current shell it seems, so not fully reliable
+    env::var("SHELL").ok()
+}
+
+fn get_default_shell_from_fs() -> Option<String> {
+    let mut user = env::var("USER").ok()?;
+    user.push(':'); // passwd file separator
+    let file = std::fs::File::open("/etc/passwd").ok()?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line.ok()?;
+        if line.starts_with(&user) {
+            // found it
+            let shell = line.split(':').nth(6)?; // the shell column
+            return Some(shell.to_owned());
+        }
+    }
+    None
+}
 
 fn get_default_shell() -> Option<String> {
-    let raw_path = env::var("SHELL").ok()?;
+    let raw_path = get_default_shell_from_fs()
+        .or_else(|| get_default_shell_from_env())?;
     let path = Path::new(&raw_path);
     let fname_osstr = path.file_name()?;
     return Some(fname_osstr.to_str()?.to_owned());
